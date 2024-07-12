c8150cc4b537bc9776d7d7ea284300c7f195ff77
KimJeongJin <128561413+wjdwls8520@users.noreply.github.com>
2024-07-12 23:18:58 +0900

    ToastUi 삭제 ( react 18 버전에서 사용 불가 )

--- app/component/ToastUi.js
+++ app/component/ToastUi.js
@@ -1,29 +1,35 @@
-'use client'
+// 'use client'
 
-import dynamic from 'next/dynamic';
-import { Editor } from '@toast-ui/react-editor';
-import '@toast-ui/editor/toastui-editor.css';
+// import dynamic from 'next/dynamic';
+// import { Editor } from '@toast-ui/react-editor';
+// import '@toast-ui/editor/toastui-editor.css';
+
+// export default function ToastUiEditor() {
+//     const ToastEditor = dynamic(() => import('@/src/components/Write/editor'), {
+//         ssr: false,
+//     });
+//     // editor.getHtml()을 사용해서 에디터 내용 받아오기
+//     // see1 = function() {
+//     //     console.log(editor.getHTML());
+//     // }
+//     // see2 = function() {
+//     //     console.log(editor.getMarkdown());
+//     // }
+
+//     return (
+//         <Editor
+//             initialValue="hello react editor world!"
+//             previewStyle="vertical"
+//             height="500px"
+//             initialEditType="wysiwyg"
+//             useCommandShortcut={false}
+//             hideModeSwitch
+//         />
+//     )
+// };
 
 export default function ToastUiEditor() {
-    const ToastEditor = dynamic(() => import('@/src/components/Write/editor'), {
-        ssr: false,
-    });
-    // editor.getHtml()을 사용해서 에디터 내용 받아오기
-    // see1 = function() {
-    //     console.log(editor.getHTML());
-    // }
-    // see2 = function() {
-    //     console.log(editor.getMarkdown());
-    // }
-
     return (
-        <Editor
-            initialValue="hello react editor world!"
-            previewStyle="vertical"
-            height="500px"
-            initialEditType="wysiwyg"
-            useCommandShortcut={false}
-            hideModeSwitch
-        />
+        <textarea id="inpTTA" className="myTTA bdCustom" placeholder="내용을 입력해주세요."></textarea>
     )
 };
