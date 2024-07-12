3b102740e79e9a0721c857399f9977905af79c8d
KimJeongJin <128561413+wjdwls8520@users.noreply.github.com>
2024-07-12 23:07:00 +0900

    ToastUi 재수정

--- app/component/ToastUi.js
+++ app/component/ToastUi.js
@@ -1,9 +1,13 @@
+'use client'
+
+import dynamic from 'next/dynamic';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/toastui-editor.css';
 
 export default function ToastUiEditor() {
-
-
+    const ToastEditor = dynamic(() => import('@/src/components/Write/editor'), {
+        ssr: false,
+    });
     // editor.getHtml()을 사용해서 에디터 내용 받아오기
     // see1 = function() {
     //     console.log(editor.getHTML());
