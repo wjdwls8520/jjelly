b6290d46d822bb37368619902dc8971e12595967
KimJeongJin <128561413+wjdwls8520@users.noreply.github.com>
2024-07-23 17:46:21 +0900

    인기모임 수정중

--- app/component/SlideFullBoard.js
+++ app/component/SlideFullBoard.js
@@ -4,9 +4,10 @@ import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { usePathname } from "next/navigation";
+import { Suspense } from "react";
 
 /* 분류 : 지역 */
-export default function SlideFullBoard(props) {
+function SlideFullBoardContents(props) {
 
     let pathName = usePathname();
 
@@ -26,90 +27,105 @@ export default function SlideFullBoard(props) {
 
                 : 
 
+                    // 자동슬라이드 + 페이지네이션 + 타이틀
                     <Swiper 
-                        slidesPerView={2.16}
-                        spaceBetween={14}
-                        className="mySwiper regionSwiper"
+                        slidesPerView={1}
+                        spaceBetween={20}
+                        className="mySwiper fullSlideBoard"
                     >
-                    <SwiperSlide className="squareBoardWrapSlide">
-                        <div className="userPost">
-                            <div className="userProfile">
-                                <img className="postProfile" src="/profile2.png" />
+                        <SwiperSlide className="fullSlideBoardSlide">
+                            <div className="meetPost">
+                                <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
+                                    <img className="postImg" src="/userimg_002.png" />
+                                </Link>
+                                <div className="postContent">
+                                    <span className="txt">
+                                        우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
+                                    </span>
+                                </div>
+                                <div className="meetProfile">
+                                    <img className="postProfile" src="/profile2.png" />
+                                    <span className="meetName">배리배리강아지모임</span>
+                                </div>
                             </div>
-                            <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
-                                <img className="postImg" src="/userimg_002.png" />
-                            </Link>
-                            <div className="postContent">
-                                <span className="txt">
-                                    우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
-                                </span>
+                        </SwiperSlide>
+                        <SwiperSlide className="fullSlideBoardSlide">
+                            <div className="meetPost">
+                                <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
+                                    <img className="postImg" src="/userimg_004.png" />
+                                </Link>
+                                <div className="postContent">
+                                    <span className="txt">
+                                        우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
+                                    </span>
+                                </div>
+                                <div className="meetProfile">
+                                    <img className="postProfile" src="/userimg_006.png" />
+                                    <span className="meetName">배리배리강아지모임</span>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="squareBoardWrapSlide">
-                        <div className="userPost">
-                            <div className="userProfile">
-                                <img className="postProfile" src="/userimg_012.png" />
+                        </SwiperSlide>
+                        <SwiperSlide className="fullSlideBoardSlide">
+                            <div className="meetPost">
+                                <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
+                                    <img className="postImg" src="/userimg_008.png" />
+                                </Link>
+                                <div className="postContent">
+                                    <span className="txt">
+                                        우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
+                                    </span>
+                                </div>
+                                <div className="meetProfile">
+                                    <img className="postProfile" src="/userimg_011.png" />
+                                    <span className="meetName">배리배리강아지모임</span>
+                                </div>
                             </div>
-                            <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
-                                <img className="postImg" src="/userimg_003.png" />
-                            </Link>
-                            <div className="postContent">
-                                <span className="txt">
-                                    우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
-                                </span>
+                        </SwiperSlide>
+                        <SwiperSlide className="fullSlideBoardSlide">
+                            <div className="meetPost">
+                                <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
+                                    <img className="postImg" src="/userimg_011.png" />
+                                </Link>
+                                <div className="postContent">
+                                    <span className="txt">
+                                        우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
+                                    </span>
+                                </div>
+                                <div className="meetProfile">
+                                    <img className="postProfile" src="/userimg_009.png" />
+                                    <span className="meetName">배리배리강아지모임</span>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="squareBoardWrapSlide">
-                        <div className="userPost">
-                            <div className="userProfile">
-                                <img className="postProfile" src="/userimg_008.png" />
+                        </SwiperSlide>
+                        <SwiperSlide className="fullSlideBoardSlide">
+                            <div className="meetPost">
+                                <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
+                                    <img className="postImg" src="/userimg_009.png" />
+                                </Link>
+                                <div className="postContent">
+                                    <span className="txt">
+                                        우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
+                                    </span>
+                                </div>
+                                <div className="meetProfile">
+                                    <img className="postProfile" src="/userimg_007.png" />
+                                    <span className="meetName">배리배리강아지모임</span>
+                                </div>
                             </div>
-                            <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
-                                <img className="postImg" src="/userimg_004.png" />
-                            </Link>
-                            <div className="postContent">
-                                <span className="txt">
-                                    우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
-                                </span>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="squareBoardWrapSlide">
-                        <div className="userPost">
-                            <div className="userProfile">
-                                <img className="postProfile" src="/userimg_009.png" />
-                            </div>
-                            <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
-                                <img className="postImg" src="/userimg_005.png" />
-                            </Link>
-                            <div className="postContent">
-                                <span className="txt">
-                                    우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
-                                </span>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="squareBoardWrapSlide">
-                        <div className="userPost">
-                            <div className="userProfile">
-                                <img className="postProfile" src="/profile.png" />
-                            </div>
-                            <Link className="imgWrap" href={{ pathname: pathName + '/detail', query: 'type=' + props.type }}>
-                                <img className="postImg" src="/userimg_011.png" />
-                            </Link>
-                            <div className="postContent">
-                                <span className="txt">
-                                    우리 집의 작은 천사, 너와 함께하는 매일이 선물 같아.
-                                </span>
-                            </div>
-                        </div>
-                    </SwiperSlide>
+                        </SwiperSlide>
                    </Swiper>
 
             }
         
         </>
     );
+};
+
+
+export default function SlideFullBoard() {
+    return(
+        <Suspense fallback={<div>Loading...</div>}>
+            <SlideFullBoardContents />
+        </Suspense>
+    )
 };
