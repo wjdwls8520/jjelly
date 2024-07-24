b50d319fc5d24cc57a4e7dfb54654f098e11d304
KimJeongJin <128561413+wjdwls8520@users.noreply.github.com>
2024-07-24 14:51:06 +0900

    스크롤탑 수정

--- app/component/ScrollToTop.js
+++ app/component/ScrollToTop.js
@@ -1,16 +1,18 @@
 'use client'
 
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { useEffect } from "react";
 
 
 
 export default function ScrollToTop() {
   const { pathname } = usePathname();
+  const pathState = useState( pathname );
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathState]);
 
   return null;
 }
